14cc9ca215772487afcc101db360c42fce2342f4
Simon Gravel <simgrvl@gmail.com>
2020-10-29 00:34:17 -0400

    getStaticProps images

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,44 @@
-export default function Home() {
-	return <h1>Hello world</h1>;
+import Image from "next/image";
+
+export default function Home({ imagesArr }) {
+	return (
+		<>
+			<h1>Images</h1>
+			<div>
+				{imagesArr.map((img, index) => {
+					return (
+						<Image
+							src={img.path}
+							alt={"image" + index}
+							key={img.path}
+							width={img.w}
+							height={img.h}
+						/>
+					);
+				})}
+			</div>
+		</>
+	);
+}
+
+export async function getStaticProps() {
+	const fs = require("fs");
+	const sizeOf = require("image-size");
+	const imagesArr = fs
+		//Loops through images folder
+		.readdirSync("./public/images", { withFileTypes: true })
+		.map((item) => {
+			//Image name and extension
+			const name = item.name;
+			//Get the dimensions of image
+			const dimensions = sizeOf("./public/images/" + name);
+			return {
+				path: "/images/" + name,
+				h: dimensions.height,
+				w: dimensions.width,
+			};
+		});
+	return {
+		props: { imagesArr },
+	};
 }
